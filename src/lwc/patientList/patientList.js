4d4e1c91164b1339a547483307126b1daf59f75a
je-lwc <79720377+je-lwc@users.noreply.github.com>
2021-03-01 14:42:31 -0500

    Update patientList.js

--- src/lwc/patientList/patientList.js
+++ src/lwc/patientList/patientList.js
@@ -1,7 +1,6 @@
+// Modify the component below to call apex [PatientController.getPatientList] imperatively and receive data.
 import { LightningElement } from "lwc";
-import getPatientList from '@salesforce/apex/PatientController.getPatientList';
 
-// ^ The apex has been imported for you. Modify the class below:
 export default class App extends LightningElement {
   
   constructor() {
