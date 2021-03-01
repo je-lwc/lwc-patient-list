57f90bbc9d7089c219466f263ea9dcd48575fc1e
je-lwc <hyu@justenergy.com>
2021-03-01 18:17:35 +0000

    First commit

---
+++ src/lwc/patientList/patientList.js
@@ -0,0 +1,11 @@
+import { LightningElement } from "lwc";
+import getPatientList from '@salesforce/apex/PatientController.getPatientList';
+
+// ^ The apex has been imported for you. Modify the class below:
+export default class App extends LightningElement {
+  
+  constructor() {
+    super();
+  }
+  
+}
